992d577625a2fbe17ad2392d27dad9d6598d5fdd
sharif-hosen <mdsharifkhan762@gmail.com>
2022-11-15 22:05:34 +0600

    complete normal routing

--- Index.php
+++ Index.php
@@ -13,14 +13,29 @@ $page_url = $_SERVER[ "REQUEST_URI"];
 
 // routing
 //--------------
-if (file_exists(__DIR__.$page_url.".php")){
-    include(__DIR__.$page_url.".php");
-}else{
-    echo "404 not found";
+// if (file_exists(__DIR__.$page_url.".php")){
+//     include(__DIR__.$page_url.".php");
+// }else{
+//     echo "404 not found";
+// }
+
+// if (file_exists(__DIR__."/resource/views".$page_url.'.php'))
+// {
+//     include(__DIR__."/resource/views".$page_url.".php");
+// }
+// else{
+//     echo"dur hoo beta";
+// }
+$page_view=__DIR__."/resource/views".$page_url.'.php';
+
+if (file_exists($page_view))
+{
+    include($page_view);
+}
+else{
+    echo"dur hoo beta";
 }
 
-
-
-dd($_SERVER,$page_url,__DIR__,file_exists(__DIR__."/about.php"));
+// dd($_SERVER,$page_url,__DIR__,file_exists(__DIR__."/about.php"));
 
 ?>
