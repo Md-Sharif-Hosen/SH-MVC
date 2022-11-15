d6a125d69e448039077b40f1ce498632d41b0dda
sharif-hosen <mdsharifkhan762@gmail.com>
2022-11-15 21:22:34 +0600

    many page Routing

--- Index.php
+++ Index.php
@@ -1,9 +1,26 @@
 <?php
 include_once("./vendor/autoload.php");
-$arr=[
-    12,3144,34343,3454545,453445,4.5,34344
-];
-   
-dd($arr);
+$page_url = $_SERVER[ "REQUEST_URI"];
+
+// routing
+//---------------------
+// if($page_url === "/contact"){
+//     include_once("./contact.php");
+// }
+// if($page_url === "/about"){
+//     include_once("./about.php");
+// }
+
+// routing
+//--------------
+if (file_exists(__DIR__.$page_url.".php")){
+    include(__DIR__.$page_url.".php");
+}else{
+    echo "404 not found";
+}
+
+
+
+dd($_SERVER,$page_url,__DIR__,file_exists(__DIR__."/about.php"));
 
 ?>
