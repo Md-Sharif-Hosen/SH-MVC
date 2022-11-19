1a70e29767d618e69a9cdf838e96a97f0b428248
sharif-hosen <mdsharifkhan762@gmail.com>
2022-11-20 04:06:47 +0600

    boostrap design

--- Index.php
+++ Index.php
@@ -3,73 +3,7 @@ include_once("./vendor/autoload.php");
 include_once('./App/helper.php');
 include_once("./routes/web.php");
 
-
-
-// class caculation{
-//     public $result=0;
-//     public function __construct($number)
-//     {
-//         $this->result +=$number;
-     
-//     }
-//     public function add($number){
-//         $this->result +=$number;
-//         return $this;
-//     }
-//         public function sub($number){
-//             $this->result -=$number;
-//             return $this;
-//         }   
-
-//         public function mul($number){
-//             $this->result *=$number;
-//             return $this;
-//         }  
-// }
-// $cals= new caculation(10);
-// $cals->add(5)->sub(2)->mul(2);  
-
-// dd($cals);
-
-class User{
-    public $info=[
-        'name'=>null,
-        'address'=>null,
-        'country'=>null,
-        'contact'=>null,
-    ];
-    public function name($name)
-    {
-        $this->info["name"]=$name;
-        return $this;
-    }
-    public function address($address)
-    {
-        $this->info["address"]=$address;
-        return $this;
-    }
-    public function country($country)
-    {
-        $this->info["country"]=$country;
-        return $this;
-    }
-    public function contact($contact)
-    {
-        $this->info["contact"]=$contact;
-        return $this;
-    }
-}
-$student  =new User();
-$student->name('sharif')->contact('01753553')->address('dhaka')->country('bd');
-
-$student2  =new User();
-$student2->name('sharif3')->contact('017535f53')->address('dhaka')->country('bd');
-
-dd($student,$student2);
-
 // dd(
 // request()->district, 
 // $_REQUEST['class']
 // );
-
-?>
--- resource/views/home.php
+++ resource/views/home.php
@@ -1,1 +1,124 @@
-<h1>Home page</h1>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC </title>
+    <link rel="stylesheet" href="/Public//CSS/bootstrap.min.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled">Disabled</a>
+                    </li>
+                </ul>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <section>
+        <div class="container">
+            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="/Public/Images/banner1.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/Public/Images/banner2.jfif" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="/Public/Images/banner3.jpg" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </section>
+    <section class="py-5 my-5">
+        <div class="container">
+            <div class="text-center">
+                <h1>Service</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, odit!</p>
+            </div>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/Public/Images/card1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/Public/Images/card1.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="/Public/Images/card3.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <script src="/Public/JS/bootstrap.min.js"></script>
+    <footer class="bg-dark text-light">
+        <div class="container text-center">
+            <p class="mb-0 py-2">copuright &copy; 2022 Dream IT</p>
+        </div>
+    </footer>
+</body>
+
+</html>
